0343e82dcc091bfbb0d2dc3aa16aa526d6b6f736
Theresa Newman <theresa.webdev@gmail.com>
2020-08-27 20:23:13 -0400

    IIFE to create human

--- app.js
+++ app.js
@@ -1,3 +1,89 @@
+const dinos = [
+    {
+        "species": "Triceratops",
+        "weight": 13000,
+        "height": 114,
+        "diet": "herbivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "First discovered in 1889 by Othniel Charles Marsh"
+    },
+    {
+        "species": "Tyrannosaurus Rex",
+        "weight": 11905,
+        "height": 144,
+        "diet": "carnivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "The largest known skull measures in at 5 feet long."
+    },
+    {
+        "species": "Anklyosaurus",
+        "weight": 10500,
+        "height": 55,
+        "diet": "herbivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "Anklyosaurus survived for approximately 135 million years."
+    },
+    {
+        "species": "Brachiosaurus",
+        "weight": 70000,
+        "height": "372",
+        "diet": "herbivore",
+        "where": "North America",
+        "when": "Late Jurasic",
+        "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+    },
+    {
+        "species": "Stegosaurus",
+        "weight": 11600,
+        "height": 79,
+        "diet": "herbivore",
+        "where": "North America, Europe, Asia",
+        "when": "Late Jurasic to Early Cretaceous",
+        "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+    },
+    {
+        "species": "Elasmosaurus",
+        "weight": 16000,
+        "height": 59,
+        "diet": "carnivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+    },
+    {
+        "species": "Pteranodon",
+        "weight": 44,
+        "height": 20,
+        "diet": "carnivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+    },
+    {
+        "species": "Pigeon",
+        "weight": 0.5,
+        "height": 9,
+        "diet": "herbivore",
+        "where": "World Wide",
+        "when": "Holocene",
+        "fact": "All birds are living dinosaurs."
+    }
+];
+function randomizeDinos(dinos) {
+  for (i = dinos.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = dinos[i]
+    dinos[i] = dinos[j]
+    dinos[j] = temp
+  }
+  return dinos;
+}
+
+
+
 // Create Dino Constructor
 function Dino( species, weight, height, diet, where, when, fact ) {
   this.species = species;
@@ -9,42 +95,37 @@ function Dino( species, weight, height, diet, where, when, fact ) {
   this.fact = fact;
 }
 
-// Create Human Object
-function Human( name, weight, feet, inches, diet ) {
-  this.name = name;
-  this.weight = weight;
-  this.height = feet * 12 + inches;
-  this.diet = diet;
-}
+// // Create Human Object
+// function Human( name, weight, feet, inches, diet ) {
+//   this.name = name;
+//   this.weight = weight;
+//   this.height = feet * 12 + inches;
+//   this.diet = diet;
+// }
 // Use IIFE to get human data from form
-(function() {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = (e) => {
-    if (xhr.readyState !== 4) {
-      return;
-    }
+function Human ( name, weight, feet, inches, diet ) {
+    this.name = name;
+    this.weight = weight;
+    this.height = feet * 12 + inches;
+    this.diet = diet;
+};
 
-    if (xhr.status === 200) {
-      //console.log('SUCCESS', xhr.responseText);
-      let response = JSON.parse(xhr.responseText);
-      let dinos = response.Dinos;
-      var i = 0, j = 0, temp = null
-      for (i = dinos.length - 1; i > 0; i -= 1) {
-        j = Math.floor(Math.random() * (i + 1))
-        temp = dinos[i]
-        dinos[i] = dinos[j]
-        dinos[j] = temp
-      }
-      console.log('init dinos',dinos);
-      var randomDinos = dinos;
-    } else {
-      console.warn('request_error');
-    }
-  };
-  xhr.open('GET', 'dino.json');
-  xhr.send();
-})();
 
+const createHuman = function( you ) {
+  (function(you) {
+    const form = document.getElementById('dino-compare');
+    let data = new FormData(form);
+    let entries = Object.fromEntries(new FormData(form));
+    //validation
+    if (!(entries.name)|| !(entries.weight) || !(entries.feet) || !(entries.diet)) {
+      return false;
+    }
+    you.name = entries.name;
+    you.weight = parseInt(entries.weight);
+    you.height = parseInt(entries.feet) * 12 + parseInt(entries.inches);
+    you.diet = entries.dies;
+  })(you);
+};
 
 
 // Create Dino Compare Method 1
@@ -71,13 +152,9 @@ const submit = document.getElementById('btn');
 
 submit.addEventListener('click', event => {
   event.preventDefault();
-  const form = document.getElementById('dino-compare');
-  let data = new FormData(form);
-  let human = Object.fromEntries(new FormData(form));
-  if (!(human.name)|| !(human.weight) || !(human.feet) || !(human.diet)) {
-    return false;
-  }
-  let you = new Human( human.name, parseInt(human.weight), parseInt(human.feet), parseInt(human.inches), human.diet );
-  // Create Dino Objects
+  let you = new Human();
+  createHuman(you);
+  console.log('you',you);
+  let randomDinos = randomizeDinos(dinos);
   console.log('randos',randomDinos);
 });
